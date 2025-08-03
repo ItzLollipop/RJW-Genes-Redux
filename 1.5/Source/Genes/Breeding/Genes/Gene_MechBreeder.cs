ef303de39aaff9f95d8ad21c0404aed2350437a7
Lollipop <56864927+ItzLollipop@users.noreply.github.com>
2025-08-02 23:53:18 -0400

    1.6 Modularization

--- 1.5/Source/Genes/Breeding/Genes/Gene_MechBreeder.cs
+++
@@ -1,9 +0,0 @@
-﻿using Verse;
-
-namespace Genes.Breeding
-{
-    public class Gene_MechBreeder : Gene
-    {
-        // This one does not do anything, the patch is some where else checking for the pawn to have this Gene!
-    }
-}
